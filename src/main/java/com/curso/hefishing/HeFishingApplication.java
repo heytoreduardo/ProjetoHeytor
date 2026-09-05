44880147026ea5623e5ba72adb070c9411d75ab0
Allan Lopes Albino <allanlopes0100@gmail.com>
2026-09-05 13:15:20 -0300

    feat: cria projeto HeFishing e endpoint de health

---
+++ src/main/java/com/curso/hefishing/HeFishingApplication.java
@@ -0,0 +1,13 @@
+package com.curso.hefishing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeFishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeFishingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/hefishing/HeFishingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.hefishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeFishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
